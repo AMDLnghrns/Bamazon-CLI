2a3c1c804d15224e19a1db5a66abbc189554ba2d
AMDLnghrns <aduncan001@gmail.com>
2019-05-17 19:29:04 -0500

    built the database

---
+++ BamazonCustomer.js
@@ -0,0 +1,109 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "$Wordf1sh211986",
+  database: "bamazon"
+});
+
+
+// id's, names, and prices
+
+function readValues() {
+  connection.query(
+    // "SELECT DISTINCT item_name FROM products WHERE ?",
+    "SELECT * FROM products",
+      {
+        genre: 'Electric'
+      },
+    function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      
+      
+      console.log(res);
+
+
+
+
+      connection.end();
+    }
+  );
+}
+
+readValues();
+
+// function updateValues(response) {
+//   var query = connection.query(
+//     "UPDATE products SET ? WHERE ?",
+//     [
+//       {
+//         price: priceInput
+//       },
+//       {
+//         product_name: productInput
+//       }
+//     ],
+//     function(err, res) {
+//       readValues();
+//     }
+//   );
+// }
+
+// inquirer
+//   .prompt([
+//     {
+//       type: "list",
+//       name: "doWhat",
+//       message: "What would you like to do?",
+//       choices: ["Add", "Bid", "Exit"]
+//     }
+//   ])
+//   .then(function(response) {
+//     if (response.doWhat === "Add") {
+//       inquirer
+//         .prompt([
+//           {
+//             type: "input",
+//             name: "productInput",
+//             message: "What would you like to add?"
+//           },
+//           {
+//             type: "input",
+//             name: "departmentInput",
+//             message: "Which department?"
+//           },
+//           {
+//             type: "input",
+//             name: "priceInput",
+//             message: "Price?"
+//           }
+//         ])
+//         .then(function createValues(response2) {
+//           var query = connection.query(
+//             "INSERT INTO products SET ?",
+//             {
+//               product_name: response2.productInput,
+//               department_name: response2.departmentInput,
+//               price: response2.priceInput
+//             },
+//             function(err, res) {
+//               readValues();
+//             }
+//           );
+//         });
+//     } else if (response.doWhat === "Bid") {
+//         console.log("Bid");
+//     } else {
+//         console.log("Come again!");
+//     }
+//   });
---
+++ bamazon.sql
@@ -0,0 +1,45 @@
+-- create database bamazon -- create the database namned bamazon 
+-- use bamazon --switch to the database
+
+-- DROP table products;
+
+-- create table products ( -- create the table with these columns
+-- 	item_id integer not null auto_increment primary key, -- integer column with an item number
+-- 	item_name varchar(50) not null, -- name of the album
+--     genre varchar(50) not null, -- genre
+--     department_name varchar(50) not null, -- department the product should be part of
+--     price decimal(13,4) not null, -- cost to customer
+--     stock_quantity integer(10) -- how much of the product you have in stock
+-- );
+
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('Dressed to Kill by KISS','Classic Rock','Electronics',25,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('Dynasty by KISS','Classic Rock','Electronics',11,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('The Dark Side of the Moon by Pink Floyd','Classic Rock','Electronics',25,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('The Wall by Pink Floyd','Classic Rock','Electronics',11,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('Wish You Were Here by Pink Floyd','Classic Rock','Electronics',25,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('Tejas by ZZ Top','Classic Rock','Electronics',11,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('Afterburner by ZZ Top','Classic Rock','Electronics',15,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('Backstreet Boys by Backstreet Boys','Pop','Electronics',10,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('Millenium by Backstreet Boys','Pop','Electronics',10,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('No Strings Attached by NSync','Pop','Electronics',15,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('Voices by Phantogram','Electric','Electronics',25,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('Nightlife by Phantogram','Electric','Electronics',25,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('Eyelid Movies by Phantogram','Electric','Electronics',11,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('Feels Like Today by Rascal Flatts','Country','Electronics',15,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('The Bones of What You Believe by Chvrches','Electric','Electronics',12,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('Every Open Eye by Chvrches','Electric','Electronics',15,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('Love Is Dead by Chvrches','Electric','Electronics',25,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('1989 by Taylor Swift','Pop','Electronics',25,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('Reputation by Taylor Swift','Pop','Electronics',20,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('Speak Now by Taylor Swift','Pop','Electronics',20,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('Epoch by Tycho','Electric','Electronics',10,5);
+-- insert into products (item_name, genre, department_name, price, stock_quantity) values ('MELT by SHAED','Electric','Electronics',11,5)
+
+
+-- select *
+-- from products
+
+
+
+-- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$Wordf1sh211986' 
+
